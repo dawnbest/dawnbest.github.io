9e984ebddb9df11cfb4c71320f53585a2fc70796
Dawn Best <55746955+dawnbest@users.noreply.github.com>
2020-03-09 09:50:26 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -2,20 +2,7 @@
 
 gsap.to(".monster", {duration: 2, xPercent: -25, ease: 'Elastic.easeInOut'});
 gsap.from(".monban", {duration: 2, x:-100, ease: 'Bounce.out'});
+gsap.to(".awayban", {duration: 2, x:-200, ease: 'Bounce.out'});
 gsap.from("main", {opacity: 0, duration: 1, yPercent:-50});
 
 //JQuery Stuff
-
-var current_h = null;
-var current_w = null;
-
-$('.resize').hover(
-    function(){
-        current_h = $(this, 'img')[0].height;
-        current_w = $(this, 'img')[0].width;
-        $(this).animate({width: (current_w * 3), height: (current_h * 3)}, 300);
-    },
-    function(){
-        $(this).animate({width: current_w + 'px', height: current_h + 'px'}, 300);
-    }
-);
