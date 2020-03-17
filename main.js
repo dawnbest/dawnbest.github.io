6cc732edce5b6fc40419221abbd53b95bec22ecb
Dawn Best <55746955+dawnbest@users.noreply.github.com>
2020-03-17 10:28:47 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 gsap.to(".monster", {duration: 2, xPercent: -25, ease: 'Elastic.easeInOut'});
 gsap.from(".monban", {duration: 2, x:-100, ease: 'Bounce.out'});
-gsap.to(".awayban", {duration: 2, x:-200, ease: 'Bounce.out'});
+gsap.to(".monban2", {duration: 2, y: 330, ease: 'Bounce.out'});
 gsap.from("main", {opacity: 0, duration: 1, yPercent:-50});
 
 //JQuery Stuff
