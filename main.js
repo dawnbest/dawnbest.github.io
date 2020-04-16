1b95e1d78a1d12afd9268e83502f8c14d0e14ce4
Dawn Best <55746955+dawnbest@users.noreply.github.com>
2020-04-16 05:26:09 -0400

    Added files updating the color scheme

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 //GSAP Stuff
 
-gsap.to(".monster", {duration: 2, xPercent: -25, ease: 'Elastic.easeInOut'});
-gsap.from(".monban", {duration: 2, x:-100, ease: 'Bounce.out'});
-gsap.to(".monban2", {duration: 2, y: 330, ease: 'Bounce.out'});
 gsap.from("main", {opacity: 0, duration: 1, yPercent:-50});
 
 //JQuery Stuff
