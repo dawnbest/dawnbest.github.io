16a9621732276dee4518b89475e23c9dd3787bc9
Dawn Best <55746955+dawnbest@users.noreply.github.com>
2020-09-21 11:22:15 -0400

    Add files via upload
    Rework of CSS and major HTML files.

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 //GSAP Stuff
 
+gsap.to(".porttitle", {duration: 2, xPercent: -25, ease: 'Elastic.easeInOut'});
 gsap.from("main", {opacity: 0, duration: 1, yPercent:-50});
+gsap.from(".portimg", {duration: 2, x:-100, ease: 'Bounce.out'});
 
 //JQuery Stuff
