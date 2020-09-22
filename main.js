38617745e03077e9d123c0d687e33c4145e762e8
Dawn Best <55746955+dawnbest@users.noreply.github.com>
2020-09-21 22:18:06 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -3,5 +3,6 @@
 gsap.to(".porttitle", {duration: 2, xPercent: -25, ease: 'Elastic.easeInOut'});
 gsap.from("main", {opacity: 0, duration: 1, yPercent:-50});
 gsap.from(".portimg", {duration: 2, x:-100, ease: 'Bounce.out'});
+gsap.from(".skullygirl", {duration: 4, x:-800, ease: 'Bounce.out'});
 
 //JQuery Stuff
